8309c4255e96416bb229b1537f7aa3362c943d4d
Peng Zhao <pzhao@pzhao.net>
2017-06-19 13:28:29 +0000

    version 1.0.2

---
+++ inst/doc/pinyin.R
@@ -0,0 +1,6 @@
+## ---- eval=FALSE---------------------------------------------------------
+#  install.packages('pinyin')
+
+## ---- eval=FALSE---------------------------------------------------------
+#  devtools::install_github("pzhaonet/pinyin")
+
